e4b27ec9beecfe89d02bea497f3c5c7f1f99615f
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-04-29 09:59:05 +0100

    feature: ported itertools module.

---
+++ maug/_itertools.py
@@ -0,0 +1,20 @@
+import itertools
+import typing
+
+
+def take(iterable: typing.Iterable, n: int) -> typing.List:
+    """Return first n items of the iterable as a list.
+
+    Based on the method in itertools recipes in
+    https://docs.python.org/3/library/itertools.html
+    """
+    return list(itertools.islice(iterable, n))
+
+
+def repeat_items(iterable: typing.Iterable, n: int) -> typing.Iterable:
+    """Repeats each item in an iterable n times.
+
+    This function transforms ['A', 'B', 'C'] (n=2) -> ['A', 'A', 'B', 'B', 'C', 'C']
+    """
+    repeated_iterables = map(lambda x: itertools.repeat(x, n), iterable)
+    return itertools.chain.from_iterable(repeated_iterables)
