f9f2d0122be15b92a67f926b4ecf9cfd653ba054
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-12-13 15:08:14 +0000

    experiment: sentence with modification trace. (#34)

--- smaug/sentence.py
+++ smaug/sentence.py
@@ -1,12 +1,48 @@
 import dataclasses
+import functools
 
-from typing import Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
+
+from smaug import frozen
+
 
 @dataclasses.dataclass(frozen=True, eq=True, order=True)
 class SpanIndex:
 
     start: int
     end: int
+
+    def encloses(self, other: "SpanIndex") -> bool:
+        """Verifies whether this span totally encloses the other.
+
+        If a span A encloses a span B, then:
+        A.start  B.start   B.end    A.end
+        ---|--------|--------|--------|---
+        """
+        return self.start <= other.start <= other.end <= self.end
+
+    def partial_overlaps(self, other: "SpanIndex") -> bool:
+        """Verifies whether this span partially overlaps the other.
+
+        If a span A partially overlaps span B, then:
+        A.start  B.start   A.end    B.end
+        ---|--------|--------|--------|---
+        or
+        B.start  A.start   B.end    A.end
+        ---|--------|--------|--------|---
+        """
+        return (
+            self.start <= other.start <= self.end <= other.end
+            or other.start <= self.start <= other.end <= self.end
+        )
+
+    def intersects(self, other: "SpanIndex") -> bool:
+        return (
+            self.encloses(other)
+            or other.encloses(self)
+            or self.partial_overlaps(other)
+            or other.partial_overlaps(self)
+        )
 
     def __post_init__(self):
         if self.start < 0:
@@ -20,7 +56,9 @@ class SpanIndex:
     def __str__(self) -> str:
         return f"[{self.start}, {self.end}]"
 
+
 SpanIndexLike = Union[Tuple[int, int], SpanIndex]
+
 
 def promote_to_span_index(s: SpanIndexLike) -> SpanIndex:
     """Promotes a SpanIndexLike object to SpanIndex.
@@ -35,29 +73,145 @@ def promote_to_span_index(s: SpanIndexLike) -> SpanIndex:
         return s
     return SpanIndex(s[0], s[1])
 
+
 @dataclasses.dataclass(frozen=True, eq=True)
 class Modification:
+    """Stores a modification that was applied to a given sentence.
 
-    span: str
-    loc: SpanIndex
+    Attributes:
+        old: The old span to be replaced by new.
+        new: The new span to replace old.
+        idx: Position where to start replacing.
+    """
+
+    old: str
+    new: str
+    idx: int
+
+    @property
+    def old_span_idx(self) -> SpanIndex:
+        return SpanIndex(self.idx, self.idx + len(self.old))
+
+    @property
+    def new_span_idx(self) -> SpanIndex:
+        return SpanIndex(self.idx, self.idx + len(self.new))
 
     def apply(self, value: str) -> str:
-        return f"{value[:self.loc.start]}{self.span}{value[self.loc.end:]}"
+        """Replaces old by new in the given string.
+
+        Args:
+            value: String to apply the modification.
+
+        Raises:
+            ValueError: If the given value does not contain old at the given index.
+
+        Returns:
+            A string with the applied modification.
+        """
+        if not value.startswith(self.old, self.idx):
+            raise ValueError(
+                f'str "{value}" does not have "{self.old}" at position {self.idx}.'
+            )
+        replace_start = self.idx
+        replace_end = replace_start + len(self.old)
+        return f"{value[:replace_start]}{self.new}{value[replace_end:]}"
+
+    def reverse(self, value: str) -> str:
+        """Reverses this modification in the given value.
+
+        This operation performs the modification in the
+        reverse direction, by replacing old by new.
+
+        Args:
+            value: String to apply the modification.
+
+        Raises:
+            ValueError: If the given value does not contain new at the given index.
+
+        Returns:
+            A string with the applied modification.
+        """
+        reverse = Modification(old=self.new, new=self.old, idx=self.idx)
+        return reverse.apply(value)
+
+
+@dataclasses.dataclass(frozen=True)
+class ModificationTrace:
+    """Stores the trace of multiple modifications in order."""
+
+    curr: Modification
+    prev: Optional["ModificationTrace"] = dataclasses.field(default=None)
+
+    def apply(self, value: str) -> str:
+        """Applies all modifications in order, from the oldest to the newest.
+
+        Args:
+            value: String to apply the modifications.
+
+        Returns:
+            Modified string.
+        """
+        modifications = self.tolist()
+        return functools.reduce(lambda acc, mod: mod.apply(acc), modifications, value)
+
+    def reverse(self, value: str) -> str:
+        """Applies all modifications in reverse order, from the newest to the oldest.
+
+        Args:
+            value: String to apply the modifications.
+
+        Returns:
+            Modified string.
+        """
+        modifications = self.tolist()
+        return functools.reduce(
+            lambda acc, mod: mod.reverse(acc), reversed(modifications), value
+        )
+
+    @staticmethod
+    def from_modifications(*modifications: Modification) -> "ModificationTrace":
+        """Constructs a modification trace by considering the modifications in order.
+
+        Raises:
+            ValueError: If no modifications were provided.
+
+        Returns:
+            The modification trace.
+        """
+        curr = None
+        for m in modifications:
+            curr = ModificationTrace(m, curr)
+        if curr is None:
+            raise ValueError("at least on modification is expected.")
+        return curr
+
+    def tolist(self) -> List[Modification]:
+        """Creates a list of the applied modifications, from oldest to newest.
+
+        Returns:
+            List with modifications.
+        """
+        modifications = []
+        self._collect_modifications(modifications)
+        return modifications
+
+    def _collect_modifications(self, acc: List[Modification]):
+        if self.prev is not None:
+            self.prev._collect_modifications(acc)
+        acc.append(self.curr)
 
 
 @dataclasses.dataclass(frozen=True)
 class Sentence:
     """Represents a sentence that stores applied modifications.
-    
-    Each sentence stores its parent sentence and the modification
-    that was applied to the parent to create this sentence.
+
+    Each sentence stores its value and the modifications trace
+    that were applied to this sentence.
     """
 
     value: str
-    
-    parent: Optional["Sentence"] = dataclasses.field(default=None)
-    
-    modification: Optional[Modification] = dataclasses.field(default=None)
+
+    trace: Optional[ModificationTrace] = dataclasses.field(default=None)
 
     def insert(self, span: str, idx: int) -> "Sentence":
         """Creates a new sentence by inserting the span at the given idx.
@@ -71,8 +225,7 @@ class Sentence:
         """
         # An insertion is a replacement of the empty string at position idx
         # by the given span.
-        loc = SpanIndex(start=idx, end=idx)
-        return self.apply_modification(Modification(span=span, loc=loc))
+        return self.apply_modification(Modification(old="", new=span, idx=idx))
 
     def delete(self, loc: SpanIndexLike) -> "Sentence":
         """Creates a new sentence by deleting the characters indexed by loc.
@@ -84,14 +237,18 @@ class Sentence:
             Sentence with the deletion operation applied.
         """
         loc = promote_to_span_index(loc)
+        to_delete = self.value[loc.start : loc.end]
         # A deletion is a replacement of the span indexed by loc with the
         # empty string.
-        return self.apply_modification(Modification(span="", loc=loc))
-    
-    def replace(self, span: str, loc: SpanIndexLike) -> "Sentence":
-        """Creates a new sentence by replacing the characters indexed by loc with the given span.
+        return self.apply_modification(
+            Modification(old=to_delete, new="", idx=loc.start)
+        )
 
-        The new sentence will have this sentence as a parent.
+    def replace(self, span: str, loc: SpanIndexLike) -> "Sentence":
+        """Creates a new sentence by replacing characters by a new span.
+
+        The new sentence will have this characters indexed by loc replaced
+        by the new span.
 
         Args:
             span: Span to insert.
@@ -101,7 +258,8 @@ class Sentence:
             Sentence with the replacement operation applied.
         """
         loc = promote_to_span_index(loc)
-        return self.apply_modification(Modification(span=span, loc=loc))
+        old = self.value[loc.start : loc.end]
+        return self.apply_modification(Modification(old=old, new=span, idx=loc.start))
 
     def apply_modification(self, modification: Modification) -> "Sentence":
         """Creates a new sentence by applying a modification to this sentence.
@@ -113,7 +271,8 @@ class Sentence:
             Sentence: The new sentence.
         """
         new_value = modification.apply(self.value)
-        return Sentence(value=new_value, parent=self, modification=modification)
+        new_trace = ModificationTrace(modification, self.trace)
+        return Sentence(value=new_value, trace=new_trace)
 
     def __str__(self) -> str:
         return self.value
--- tests/test_sentence.py
+++ tests/test_sentence.py
@@ -2,6 +2,66 @@ import pytest
 
 from smaug import sentence
 
+from typing import Optional
+
+
+@pytest.mark.parametrize(
+    "old,new,modification",
+    [
+        pytest.param(
+            'Sentence with "old text to be replaced" in the middle.',
+            'Sentence with "replaced new text" in the middle.',
+            sentence.Modification(
+                '"old text to be replaced"',
+                '"replaced new text"',
+                14,
+            ),
+            id="Modify the middle of the sentence.",
+        ),
+        pytest.param(
+            '"Sentence with old text to be replaced" in the beginning.',
+            '"Sentence with replaced new text" in the beginning.',
+            sentence.Modification(
+                '"Sentence with old text to be replaced"',
+                '"Sentence with replaced new text"',
+                0,
+            ),
+            id="Modify the beginning of the sentence.",
+        ),
+        pytest.param(
+            'Sentence with "old text to be replaced in the end".',
+            'Sentence with "replaced new text in the end".',
+            sentence.Modification(
+                '"old text to be replaced in the end".',
+                '"replaced new text in the end".',
+                14,
+            ),
+            id="Modify the end of the sentence.",
+        ),
+    ],
+)
+def test_modification(old: str, new: str, modification: sentence.Modification):
+    new_output = modification.apply(old)
+    assert new == new_output
+    old_output = modification.reverse(new_output)
+    assert old == old_output
+
+
+def test_modification_trace():
+    old = 'Original Sentence with "text to modify" and "more text to modify".'
+    new = 'Original Sentence with "modified text" and "more modifed text".'
+
+    trace = sentence.ModificationTrace.from_modifications(
+        sentence.Modification('"text to modify"', '"modified text"', 23),
+        sentence.Modification('"more text to modify"', '"more modifed text"', 43),
+    )
+
+    new_output = trace.apply(old)
+    assert new == new_output
+    old_output = trace.reverse(new_output)
+    assert old == old_output
+
+
 @pytest.mark.parametrize(
     "original,span,idx,expected",
     [
@@ -10,76 +70,95 @@ from smaug import sentence
             ", text to add at the middle,",
             17,
             sentence.Sentence(
-                "Original Sentence, text to add at the middle, without modifications.", 
-                modification=sentence.Modification(", text to add at the middle,", sentence.SpanIndex(17, 17)),
-                parent=sentence.Sentence("Original Sentence without modifications."),
+                "Original Sentence, text to add at the middle, without modifications.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification("", ", text to add at the middle,", 17),
+                ),
             ),
-            id="Middle of sentence insertion."
+            id="Middle of sentence insertion.",
         ),
         pytest.param(
             sentence.Sentence("Original Sentence without modifications."),
             "Text to add at the beginning. ",
             0,
             sentence.Sentence(
-                "Text to add at the beginning. Original Sentence without modifications.", 
-                modification=sentence.Modification("Text to add at the beginning. ", sentence.SpanIndex(0, 0)),
-                parent=sentence.Sentence("Original Sentence without modifications."),
+                "Text to add at the beginning. Original Sentence without modifications.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification("", "Text to add at the beginning. ", 0),
+                ),
             ),
-            id="Beginning of sentence insertion."
+            id="Beginning of sentence insertion.",
         ),
         pytest.param(
             sentence.Sentence("Original Sentence without modifications."),
             " Text to add at the end.",
             40,
             sentence.Sentence(
-                "Original Sentence without modifications. Text to add at the end.", 
-                modification=sentence.Modification(" Text to add at the end.", sentence.SpanIndex(40, 40)),
-                parent=sentence.Sentence("Original Sentence without modifications."),
+                "Original Sentence without modifications. Text to add at the end.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification("", " Text to add at the end.", 40),
+                ),
             ),
-            id="End of sentence replacement."
-        )
-    ]
+            id="End of sentence replacement.",
+        ),
+    ],
 )
-def test_insert(original: sentence.Sentence, span: str, idx: int, expected: sentence.Sentence):
+def test_insert(
+    original: sentence.Sentence, span: str, idx: int, expected: sentence.Sentence
+):
     output = original.insert(span, idx)
     _assert_equal_sentences(expected, output)
+
 
 @pytest.mark.parametrize(
     "original,loc,expected",
     [
         pytest.param(
-            sentence.Sentence("Original Sentence, text to delete at the middle, without modifications."),
+            sentence.Sentence(
+                "Original Sentence, text to delete at the middle, without modifications."
+            ),
             (17, 48),
             sentence.Sentence(
-                "Original Sentence without modifications.", 
-                modification=sentence.Modification("", sentence.SpanIndex(17, 48)),
-                parent=sentence.Sentence("Original Sentence, text to delete at the middle, without modifications."),
+                "Original Sentence without modifications.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification(", text to delete at the middle,", "", 17),
+                ),
             ),
-            id="Middle of sentence deletion."
+            id="Middle of sentence deletion.",
         ),
         pytest.param(
-            sentence.Sentence("Text to delete at the beginning. Original Sentence without modifications."),
+            sentence.Sentence(
+                "Text to delete at the beginning. Original Sentence without modifications."
+            ),
             (0, 33),
             sentence.Sentence(
-                "Original Sentence without modifications.", 
-                modification=sentence.Modification("", sentence.SpanIndex(0, 33)),
-                parent=sentence.Sentence("Text to delete at the beginning. Original Sentence without modifications."),
+                "Original Sentence without modifications.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification("Text to delete at the beginning. ", "", 0),
+                ),
             ),
-            id="Beginning of sentence deletion."
+            id="Beginning of sentence deletion.",
         ),
         pytest.param(
-            sentence.Sentence("Original Sentence without modifications. Text to delete at the end."),
+            sentence.Sentence(
+                "Original Sentence without modifications. Text to delete at the end."
+            ),
             (40, 67),
             sentence.Sentence(
-                "Original Sentence without modifications.", 
-                modification=sentence.Modification("", sentence.SpanIndex(40, 67)),
-                parent=sentence.Sentence("Original Sentence without modifications. Text to delete at the end."),
+                "Original Sentence without modifications.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification(" Text to delete at the end.", "", 40),
+                ),
             ),
-            id="End of sentence deletion."
-        )
-    ]
+            id="End of sentence deletion.",
+        ),
+    ],
 )
-def test_deletion(original: sentence.Sentence, loc: sentence.SpanIndexLike, expected: sentence.Sentence):
+def test_deletion(
+    original: sentence.Sentence,
+    loc: sentence.SpanIndexLike,
+    expected: sentence.Sentence,
+):
     output = original.delete(loc)
     _assert_equal_sentences(expected, output)
 
@@ -89,53 +168,68 @@ def test_deletion(original: sentence.Sentence, loc: sentence.SpanIndexLike, expe
     [
         pytest.param(
             sentence.Sentence("Original Sentence without modifications."),
-            ", text to replace without,",
+            ', text to replace " without",',
             (17, 25),
             sentence.Sentence(
-                "Original Sentence, text to replace without, modifications.", 
-                modification=sentence.Modification(", text to replace without,", sentence.SpanIndex(17, 25)),
-                parent=sentence.Sentence("Original Sentence without modifications."),
+                'Original Sentence, text to replace " without", modifications.',
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification(
+                        " without", ', text to replace " without",', 17
+                    ),
+                ),
             ),
-            id="Middle of sentence replacement."
+            id="Middle of sentence replacement.",
         ),
         pytest.param(
             sentence.Sentence("Original Sentence without modifications."),
             "Text to replace Original.",
             (0, 8),
             sentence.Sentence(
-                "Text to replace Original. Sentence without modifications.", 
-                modification=sentence.Modification("Text to replace Original.", sentence.SpanIndex(0, 8)),
-                parent=sentence.Sentence("Original Sentence without modifications."),
+                "Text to replace Original. Sentence without modifications.",
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification("Original", "Text to replace Original.", 0),
+                ),
             ),
-            id="Beginning of sentence replacement."
+            id="Beginning of sentence replacement.",
         ),
         pytest.param(
             sentence.Sentence("Original Sentence without modifications."),
-            ". Text to replace modifications.",
+            '. Text to replace " modifications.".',
             (25, 40),
             sentence.Sentence(
-                "Original Sentence without. Text to replace modifications.", 
-                modification=sentence.Modification(". Text to replace modifications.", sentence.SpanIndex(25, 40)),
-                parent=sentence.Sentence("Original Sentence without modifications."),
+                'Original Sentence without. Text to replace " modifications.".',
+                trace=sentence.ModificationTrace.from_modifications(
+                    sentence.Modification(
+                        " modifications.", '. Text to replace " modifications.".', 25
+                    ),
+                ),
             ),
-            id="End of sentence replacement."
-        )
-    ]
+            id="End of sentence replacement.",
+        ),
+    ],
 )
 def test_replace(
-    original: sentence.Sentence, 
-    span: str, 
-    loc: sentence.SpanIndexLike, 
+    original: sentence.Sentence,
+    span: str,
+    loc: sentence.SpanIndexLike,
     expected: sentence.Sentence,
 ):
     output = original.replace(span, loc)
     _assert_equal_sentences(expected, output)
 
+
 def _assert_equal_sentences(expected: sentence.Sentence, actual: sentence.Sentence):
     assert expected.value == actual.value
-    assert expected.modification == actual.modification
-    if expected.parent is None:
-        assert actual.parent is None
+    _assert_equal_traces(expected.trace, actual.trace)
+
+
+def _assert_equal_traces(
+    expected: Optional[sentence.ModificationTrace],
+    actual: Optional[sentence.ModificationTrace],
+):
+    if expected is None:
+        assert actual is None
     else:
-        assert actual.parent is not None
-        _assert_equal_sentences(expected.parent, actual.parent)
+        assert actual is not None
+        assert expected.curr == actual.curr
+        _assert_equal_traces(expected.prev, actual.prev)
