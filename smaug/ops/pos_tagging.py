13c90865fe8634e798c65a92149eb306af698f2f
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-12-21 17:23:53 +0000

    experiment: stanza pos tagger with sentence like input. (#34)

--- smaug/ops/pos_tagging.py
+++ smaug/ops/pos_tagging.py
@@ -1,11 +1,13 @@
 import stanza
 
 from smaug import core
+from smaug import sentence
 
 
 def stanza_pos_predict(
-    text: core.DataLike[str], pos_pipeline: stanza.Pipeline
+    text: core.DataLike[sentence.SentenceLike], pos_pipeline: stanza.Pipeline
 ) -> core.Data:
     """Predicts part-of-speech tags with stanza POS model."""
     text = core.promote_to_data(text)
-    return core.Data(pos_pipeline(t) for t in text)
+    sentences = [sentence.promote_to_sentence(t) for t in text]
+    return core.Data(pos_pipeline(s.value) for s in sentences)
